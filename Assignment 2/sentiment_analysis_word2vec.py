cbef119e78c86431488ec2e53d4cc554762a7a92
Liam Bui Le Linh <Liam Bui Le Linh>
2017-01-18 19:47:13 -0800

    Assignment

---
+++ Assignment 2/sentiment_analysis_word2vec.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import Word2Vec, RegexTokenizer, StopWordsRemover
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+import sys
+ 
+def main(argv=None):
+    if argv is None:
+        inputs_train = sys.argv[1]
+        inputs_test = sys.argv[2]
+    
+    conf = SparkConf().setAppName('sentiment-analysis-word2vec')
+    sc = SparkContext(conf=conf)
+    sqlCt = SQLContext(sc)
+    
+    #read train json file and prepare data (label, feature)
+    text = sqlCt.read.json(inputs_train)
+    train = text.select('overall','reviewText').withColumnRenamed('overall','label')
+
+    ## ML PIPELINE:
+    # Split at whitespace and characters that are not letter
+    tokenizer = RegexTokenizer(inputCol="reviewText", outputCol="words", pattern="\\P{Alpha}+")
+    
+    # stopword remover
+    remover = StopWordsRemover(inputCol="words", outputCol="filtered_words")
+    
+    # Word2Vec Features
+    word2Vec = Word2Vec(inputCol="filtered_words", outputCol="features")
+    
+    # linear Regression Model
+    lr = LinearRegression(maxIter=20, regParam=0.1)
+    # Final Pipeline
+    pipeline = Pipeline(stages=[tokenizer, remover, word2Vec, lr])
+    
+    # FIT MODEL USING CROSS VALIDATION
+    # Parameter grid for cross validation: numFeatures and regParam
+    paramGrid = ParamGridBuilder() \
+        .addGrid(lr.regParam, [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) \
+        .build()
+        
+    # 5-fold cross validation
+    crossval = CrossValidator(estimator=pipeline,
+                          estimatorParamMaps=paramGrid,
+                          evaluator=RegressionEvaluator(),
+                          numFolds=2)
+    
+    # Run cross-validation, and choose the best set of parameters.
+    model = crossval.fit(train)
+    
+    ## EVALUATION
+    evaluator = RegressionEvaluator()
+    # RMSE on train data
+    prediction_train = model.transform(train)
+    rmse_train = evaluator.evaluate(prediction_train)
+
+    #read test json file and prepare data (label, feature)
+    text = sqlCt.read.json(inputs_test)
+    test= text.select('overall','reviewText').withColumnRenamed('overall','label')
+    
+    # Evaluate the model on test data
+    prediction_test = model.transform(test)
+    rmse_test = evaluator.evaluate(prediction_test)
+    
+    # Print Result
+    print("MODEL WITH Word2Vec features:")    
+    print("Train RMSE: %f" % rmse_train)
+    print("Test RMSE: %f" % rmse_test)
+
+if __name__ == "__main__":
+    main()
+
