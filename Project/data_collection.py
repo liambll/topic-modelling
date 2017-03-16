806f7be13baad27a84198fb61953797fefd02c0d
Liam Bui Le Linh <Liam Bui Le Linh>
2017-03-15 18:05:23 -0700

    update data_collection web crawler

---
+++ Project/data_collection.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 11:18:46 2017
+
+@author: linhb
+"""
+
+import os
+from pymongo import MongoClient
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import data_processing
+
+class PDF_File(scrapy.Item):
+    source = scrapy.Field()
+    title = scrapy.Field()
+    authors = scrapy.Field()
+    abstract = scrapy.Field()
+    keywords = scrapy.Field()
+    content = scrapy.Field()
+    
+class Spider_JMLR(scrapy.Spider):
+    name = "JMLR"
+
+    def start_requests(self):
+        urls = []
+        # JMLR papers in the past 5 years
+        for i in range(18,19,1):
+            urls.append('http://www.jmlr.org/papers/v%i/' %i)
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        self.log('Processing %s' % response.url)
+        dl_list = response.css('dl')
+        for dl in dl_list:
+            item = PDF_File()
+            item['source'] = 'JRML'
+            item['title'] = dl.css('dt::text').extract_first()
+            item['authors'] = dl.css('i::text').extract_first()
+            item['abstract'] = ''
+            item['keywords'] = ''
+            item['content'] = ''
+            
+            pdf_url = dl.css('a::attr(href)').extract()[1]
+            pdf_fullurl = response.urljoin(pdf_url)
+            request = scrapy.Request(pdf_fullurl, callback=self.processPDF)
+            request.meta['item'] = item
+            yield request   
+            
+    def processPDF(self, response):
+        # save pdf file
+        path = response.url.split('/')[-1]
+        
+        f = open(path, 'wb')
+        f.write(response.body)
+        f.close()
+        # Process PDF file
+        text = data_processing.pdf_to_text(path) #Something wrong here!
+        os.remove(f.name)
+    
+        
+        # Save Item object to MongoDB
+        item = response.meta['item']
+        paper = {"_id": item['source']+' '+item['title'],
+                 "source": item['source'],
+                 "title": item['title'],
+                 "authors": item['authors'],
+                 "abstract": item['abstract'],
+                 "keywords": item['keywords'],
+                 "content": response,
+                 "text": text
+                 }
+        collection.insert_one(paper)
+        
+        
+
+# Setup MongoDB Connection
+# Start MongoDB Server: mongod.exe --dbpath D:\Training\Software\MongoDB\data
+# export PATH=/home/llbui/mongodb/mongodb-linux-x86_64-3.4.2/bin:$PATH
+# mongod --dbpath /home/llbui/mongodb/data
+client = MongoClient("mongodb://localhost:27017")
+db = client['publications']
+collection = db['papers']
+
+'''
+f = open('14-249.pdf', 'rb')
+content = f.read()
+text = ''
+f.close()
+
+f2 = open('test.pdf', 'wb')
+f2.write(content)
+f2.close()
+
+paper = {"_id": 'JRML'+' '+'a Title'
+         "source": 'JRML',
+         "title": 'a Title',
+         "authors": 'list of authorszzzz',
+         "abstract": 'an abstract',
+         "keywords": 'list of keywordszzzz',
+         "content": content,
+         "text": text
+         }
+collection.insert_one(paper)
+'''
+
+# Start Web Crawler
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+process.crawl(Spider_JMLR)
+process.start()
+
+# Close MongoDB connection
+client.close()
--- Project/data_processing.py
+++ Project/data_processing.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-import pdfminer
+"""
+Created on Wed Mar 15 11:18:46 2017
 
+@author: linhb
+"""
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter#process_pdf
 from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
@@ -9,7 +12,6 @@ from pdfminer.layout import LAParams
 from io import StringIO
 
 def pdf_to_text(pdfname):
-
     # PDFMiner boilerplate
     rsrcmgr = PDFResourceManager()
     sio = StringIO()
@@ -34,5 +36,13 @@ def pdf_to_text(pdfname):
 
     return text
 
-pdfname = "Project/PDF/benavoli16a.pdf"
+'''
+pdfname = "http://www.jmlr.org/papers/volume18/14-249/14-249.pdf"
 text = pdf_to_text(pdfname)
+text = text.lower()
+abstract_index = text.find("abstract")
+keywords_index = text.find("keywords")
+fulltext_index = text.find("1. introduction")
+abstract = text[abstract_index: keywords_index]
+keywords = text[keywords_index: fulltext_index]
+'''
